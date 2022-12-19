406e0a33d5daa222e2b9357be885171a42b5f1bf
Dawood Altai <108373829+Altai001@users.noreply.github.com>
2022-12-20 02:56:14 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,12 @@
+question_data = [
+    {"text": "Human blood is red.", "answer": "True"}, 
+    {"text": "The larghest city of Türkiye is Bursa.", "answer": "False"},
+    {"text": "One Byte is eight bit.", "answer": "True"},
+    {"text": "Python is the best progrming language.", "answer": "True"},
+    {"text": "Chat gpt is going to rise.", "answer": "True"},
+    {"text": "html is a programing language", "answer": "False"},
+    {"text": "The loudest aniaml ;s Elephant.", "answer": "False"},
+    {"text": "10 is going to be 1010 in binary system.", "answer": "True"},
+    {"text": "English easy language to learn.", "answer": "True"},
+    {"text": "Chocolate can kill a small dog.", "answer": "True"}
+]
